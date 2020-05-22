fb5869349bd58a3bbc091c6bf3258688d6770df2
David Chopin <dchopin1@live.maryville.edu>
2020-05-21 23:59:18 -0500

    Added ModalBottomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:FinanceApp/new_transaction.dart';
 import 'package:FinanceApp/transaction.dart';
-import 'package:FinanceApp/user_transactions.dart';
+import 'package:FinanceApp/transaction_list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,10 +21,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [];
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Transaction> _transactions = [];
+
+  void _addNewTransaction(String title, double amount) {
+    final Transaction transaction = Transaction(
+        title: title,
+        amount: amount,
+        date: DateTime.now(),
+        id: "t${_transactions.length + 1}");
+    setState(() {
+      _transactions.add(transaction);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +58,10 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Flutter App"),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.add), onPressed: () {},)
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          )
         ],
       ),
       body: Column(
@@ -44,10 +74,13 @@ class MyHomePage extends StatelessWidget {
             ),
             width: double.infinity,
           ),
-          UserTransactions(),
+          TransactionList(_transactions),
         ],
       ),
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.add), onPressed: () {},),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
     );
   }
 }
